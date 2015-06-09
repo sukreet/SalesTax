3cd39b545327277dd97a67723acf152d729e5d65
sukreet <sukreetr@thoughtworks.com>
2015-06-09 17:46:28 +0530

    Sukreet : refactored code

--- Test/com/salestax/GenerateItemTest.java
+++ Test/com/salestax/GenerateItemTest.java
@@ -3,13 +3,11 @@ package com.salestax;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 public class GenerateItemTest {
 
 
-    
+
 
     @Test
     public void shouldBeAbleToCreateAnObjectOfItemClass() {
